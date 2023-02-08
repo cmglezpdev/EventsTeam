adb3a214fc0ade20cc90af25706a7064ee3c13b3
Carlos Manuel González <cmglezpdev@gmail.com>
2023-02-08 17:24:45 -0500

    feat: Add basic Events List to have a visual idea

---
+++ components/Events/EventsProfileList.tsx
@@ -0,0 +1,13 @@
+import { SmallCardEvent } from "./SmallCardEvent";
+
+export const EventsProfileList = () => {
+    return (
+        <section className="flex justify-center gap-4 flex-wrap">
+            <SmallCardEvent />
+            <SmallCardEvent />
+            <SmallCardEvent />
+            <SmallCardEvent />
+            <SmallCardEvent />
+        </section>
+    )
+}
