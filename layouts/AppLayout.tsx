dd5e9b0a361678c4413b09338f6cf2b967823f3a
Carlos Manuel González <cmglezpdev@gmail.com>
2023-02-09 00:52:15 -0500

    feat: Add App layout

---
+++ layouts/AppLayout.tsx
@@ -0,0 +1,36 @@
+import { FC, ReactNode } from 'react';
+import Head from 'next/head';
+import { NavBar } from '@/components';
+
+interface Props {
+    title: string;
+    pageDescription: string;
+    imageFullUrl?: string;
+    children: ReactNode
+}
+
+export const AppLayout:FC<Props> = ({ children, title, pageDescription, imageFullUrl }) => {
+    return (
+        <>
+            <Head>
+                <title>{ title }</title>
+                <meta name='description' content={pageDescription} />
+                <meta name='og:title' content={title} />
+                <meta name='og:description' content={pageDescription} />
+                
+                {
+                    imageFullUrl && (
+                        <meta name='og:image' content={imageFullUrl} />
+                    )
+                }
+            </Head>
+            
+            <div className="container px-4 m-auto">
+                <NavBar />
+                <main>
+                    { children }
+                </main>
+            </div>
+        </>
+    )
+}
