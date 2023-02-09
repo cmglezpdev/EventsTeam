aabcb71509c0941a216e07b488078198bf59caf2
Carlos Manuel González <cmglezpdev@gmail.com>
2023-02-09 00:35:01 -0500

    style: Fix styles of the initial page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,20 @@
-import { CardEvent, NavBar, SearchInput } from "@/components";
+import { BigCardEvent, NavBar, SearchInput } from "@/components";
 
 export default function Home() {
   return (
-    <main>
+    <main className="container px-4 m-auto">
       <NavBar />
-      <SearchInput />              
-      <div className="flex w-full justify-center gap-5">
-        <CardEvent />
-        <CardEvent />
+      <div className="min-w-[384px] max-w-xl m-auto mt-5 mb-9">
+        <SearchInput />              
+      </div>
+      <div className="flex w-full flex-wrap justify-center gap-5">
+        <BigCardEvent />
+        <BigCardEvent />
+        <BigCardEvent />
+        <BigCardEvent />
+        <BigCardEvent />
+        <BigCardEvent />
       </div>
     </main>
-  )
-}
-
-const SearchIcon = () => {
-  return (
-    <svg
-      className="w-6 h-6 cursor-pointer" 
-      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-    </svg>
   )
 }
